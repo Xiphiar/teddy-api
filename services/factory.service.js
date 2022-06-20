04a88c3bad2d8a8b2c140f3634ae6cc7235f01fe
Xiphiar <me@xiphiar.com>
2022-06-19 20:01:25 -0400

    disable permit for factory

--- services/factory.service.js
+++ services/factory.service.js
@@ -13,11 +13,12 @@ export async function factoryOrder (input) {
   const owner = input.owner.trim() || null
   if (!owner) throw "Something went wrong: Request did not include an owner address."
 
-  const signature = JSON.parse(input.signature)
+  // const signature = JSON.parse(input.signature)
 
   //verify address is not blacklisted
-  const address = base64PubkeyToAddress(signature.pub_key.value, "secret")
-  if (blacklisted.includes(address)) throw `${address} is blacklisted from this API.`
+  //const address = base64PubkeyToAddress(signature.pub_key.value, "secret")
+
+  if (blacklisted.includes(owner)) throw `${address} is blacklisted from this API.`
 
 
 
@@ -49,48 +50,48 @@ export async function factoryOrder (input) {
   if (!teddy3) throw "Something went wrong: Request did not include an ID for Teddy #3."
   if (!base) throw "Something went wrong: Request did not include a base design."
 
-  // unsigned permit to verify
-  const permitTx = {
-    chain_id: process.env.CHAIN_ID,
-    account_number: "0", // Must be 0
-    sequence: "0", // Must be 0
-    fee: {
-      amount: [{ denom: "uscrt", amount: "0" }], // Must be 0 uscrt
-      gas: "1", // Must be 1
-    },
-    msgs: [
-      {
-        type: "factory_order", // Must match the type on the client
-        value: {
-          permit_name: input.permit_name,
-          allowed_destinations: JSON.parse(input.allowed_destinations),
-          order_details: {
-            teddy1: teddy1,
-            teddy2: teddy2,
-            teddy3: teddy3,
-            owner: owner,
-            tx_hash: tx_hash,
-            base_design: base,
-            face: face,
-            color: color,
-            background: background,
-            hand: hand,
-            head: head,
-            body: body,
-            eyewear: eyewear,
-            notes: notes,
-            name: name,
-            goldToken: goldToken,
-          }
-        },
-      },
-    ],
-    memo: "" // Must be empty
-  }
-  //console.log(JSON.stringify(permitTx, null, 2));
+  // // unsigned permit to verify
+  // const permitTx = {
+  //   chain_id: process.env.CHAIN_ID,
+  //   account_number: "0", // Must be 0
+  //   sequence: "0", // Must be 0
+  //   fee: {
+  //     amount: [{ denom: "uscrt", amount: "0" }], // Must be 0 uscrt
+  //     gas: "1", // Must be 1
+  //   },
+  //   msgs: [
+  //     {
+  //       type: "factory_order", // Must match the type on the client
+  //       value: {
+  //         permit_name: input.permit_name,
+  //         allowed_destinations: JSON.parse(input.allowed_destinations),
+  //         order_details: {
+  //           teddy1: teddy1,
+  //           teddy2: teddy2,
+  //           teddy3: teddy3,
+  //           owner: owner,
+  //           tx_hash: tx_hash,
+  //           base_design: base,
+  //           face: face,
+  //           color: color,
+  //           background: background,
+  //           hand: hand,
+  //           head: head,
+  //           body: body,
+  //           eyewear: eyewear,
+  //           notes: notes,
+  //           name: name,
+  //           goldToken: goldToken,
+  //         }
+  //       },
+  //     },
+  //   ],
+  //   memo: "" // Must be empty
+  // }
+  // //console.log(JSON.stringify(permitTx, null, 2));
   
-  //verify signature
-  if (!sig.verifySignature(permitTx, signature)) throw "Provided permit was unable to be verified.";
+  // //verify signature
+  // if (!sig.verifySignature(permitTx, signature)) throw "Provided permit was unable to be verified.";
   
   //verify tx hash is not already in DB
   if (await hashInDb(tx_hash)) throw `TX Hash ${tx_hash} is already in database!`
